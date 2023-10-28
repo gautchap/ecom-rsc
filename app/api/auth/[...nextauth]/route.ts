4e72e6a39fb1a85aa37f1bb7f0bf9d10ec595c2d
gautchap <gautchap+github@outlook.com>
2023-10-28 18:37:35 +0200

    feat : add nextauth route

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,36 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import Email from "next-auth/providers/email";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import { sendVerificationRequest } from "@/template/mail";
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        Email({
+            server: {
+                host: process.env.SMTP_HOST,
+                port: Number(process.env.SMTP_PORT),
+                auth: {
+                    user: process.env.SMTP_USER,
+                    pass: process.env.SMTP_PASSWORD,
+                },
+            },
+            from: process.env.EMAIL_FROM,
+            sendVerificationRequest,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    pages: {
+        signIn: "/login",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
