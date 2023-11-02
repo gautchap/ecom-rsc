17d4c8b17c356d40c3fc18a05ab154602a69a27c
gautchap <gautchap+github@outlook.com>
2023-11-02 02:07:48 +0100

    refactor : add components in user page

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,3 +1,20 @@
-export default function Account() {
-    return <div>Account</div>;
+import EditUser from "@/components/edit-user";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getUser } from "@/db/user";
+import { redirect } from "next/navigation";
+import EditUserAddress from "@/components/edit-user-address";
+
+export default async function Account() {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/login");
+
+  const user = await getUser({ id: session.user.id });
+
+  return (
+    <>
+      <EditUser user={user} />
+      <EditUserAddress user={user} />
+    </>
+  );
 }
