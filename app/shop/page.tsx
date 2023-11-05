302eb3099ffc78459ea3145acfd21d67ea32cca8
gautchap <gautchap+github@outlook.com>
2023-11-06 00:06:30 +0100

    refactor : change ui

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import Link from "next/link";
+import { getCategories } from "@/db/categories";
 
-export default function Page() {
-  return <div>Page</div>;
+export default async function Page() {
+  const categories = await getCategories();
+  return (
+    <section>
+      {categories?.map((category) => (
+        <div key={category.id}>
+          <Link className="uppercase font-bold" href={`/shop/${category.name}`}>
+            {category.name}
+          </Link>
+          <p className="text-muted-foreground">{category.description}</p>
+        </div>
+      ))}
+    </section>
+  );
 }
