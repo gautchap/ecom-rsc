82b2aafbb1f4794afc564a80e0e9bbc3b83f2445
gautchap <gautchap+github@outlook.com>
2023-11-06 00:01:04 +0100

    feat : local storage hook

---
+++ hooks/useLocalStorage.ts
@@ -0,0 +1,22 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+  const [value, setValue] = useState<T>(() => {
+    const jsonValue = window.localStorage.getItem(key);
+    if (jsonValue != null) return JSON.parse(jsonValue);
+
+    return typeof initialValue === "function"
+      ? (initialValue as () => T)()
+      : initialValue;
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [key, value]);
+
+  return [value, setValue] as [typeof value, typeof setValue];
+}
