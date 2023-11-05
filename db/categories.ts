0fe043580d923043ef8d2e23ba4f289c259a71ab
gautchap <gautchap+github@outlook.com>
2023-11-06 00:02:17 +0100

    feat : add categories db query

--- db/categories.ts
+++ db/categories.ts
@@ -1,5 +1,33 @@
+"use server";
+
 import { prisma } from "@/lib/prisma";
-import { CategoriesWithProducts } from "@/types/categories";
+import { ProductCategory, Prisma } from "@prisma/client";
+import { CategoriesWithProducts, updateCategoryType } from "@/types/categories";
+
+export const createCategory = async ({
+  name,
+  description,
+}: updateCategoryType): Promise<ProductCategory | null> => {
+  let isCategoryCreated = null;
+
+  try {
+    isCategoryCreated = await prisma.productCategory.create({
+      data: {
+        name,
+        description,
+      },
+    });
+  } catch (error: unknown) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2002"
+    ) {
+      return null;
+    }
+  }
+
+  return isCategoryCreated;
+};
 
 export const getCategory = async (
   slug: string,
@@ -10,6 +38,64 @@ export const getCategory = async (
     },
     include: {
       Product: true,
+    },
+  });
+
+  if (!category) return null;
+
+  return category;
+};
+
+export const getCategories = async (): Promise<
+  CategoriesWithProducts[] | null
+> => {
+  const categories = await prisma.productCategory.findMany({
+    include: {
+      Product: true,
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+  });
+
+  if (!categories) return null;
+
+  return categories;
+};
+
+export const updateCategory = async ({
+  id,
+  name,
+  description,
+}: updateCategoryType): Promise<ProductCategory | null> => {
+  let isCategoryUpdated = null;
+
+  try {
+    isCategoryUpdated = await prisma.productCategory.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+        description,
+      },
+    });
+  } catch (error: unknown) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2002"
+    ) {
+      return null;
+    }
+  }
+
+  return isCategoryUpdated;
+};
+
+export const deleteCategory = async (id: string) => {
+  const category = await prisma.productCategory.delete({
+    where: {
+      id,
     },
   });
 
--- types/categories.ts
+++ types/categories.ts
@@ -1,5 +1,21 @@
+import { z } from "zod";
 import { Product, ProductCategory } from "@prisma/client";
 
 export interface CategoriesWithProducts extends ProductCategory {
   Product: Product[];
 }
+
+export const categorySchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string().optional(),
+  createdAt: z.coerce.date(),
+  updatedAt: z.coerce.date(),
+});
+
+export const updateCategorySchema = categorySchema.omit({
+  createdAt: true,
+  updatedAt: true,
+});
+
+export type updateCategoryType = z.infer<typeof updateCategorySchema>;
