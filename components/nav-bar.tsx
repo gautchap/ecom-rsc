8bfa15bec9bddde7ca266983f5c735924fda2848
gautchap <gautchap+github@outlook.com>
2024-01-14 01:04:48 +0100

    refactor: use responsive login modal in nav

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,32 +1,31 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { Profile } from "@/components/profile";
 import MainNav from "@/components/main-nav";
 import SideNav from "@/components/side-nav";
 import ThemeToggle from "@/components/theme-toggle";
 import { Session } from "next-auth";
+import { ModalLogin } from "@/components/modal-login";
 
 export type NavBarProps = {
-  session: Session | null;
+    session: Session | null;
 };
 
 export function Navbar({ session }: NavBarProps) {
-  return (
-    <nav className="container flex items-center justify-between py-3">
-      <SideNav />
-      <MainNav />
-      <div className="flex gap-2 items-center">
-        {session ? (
-          <Profile session={session} />
-        ) : (
-          <Link href="/login">
-            <Button>Connexion</Button>
-          </Link>
-        )}
-        <div className="flex items-center space-x-2">
-          <ThemeToggle />
-        </div>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="container flex items-center justify-between py-3">
+            <SideNav />
+            <MainNav />
+            <div className="flex gap-2 items-center">
+                {session ? (
+                    <Profile session={session} />
+                ) : (
+                    <>
+                        <ModalLogin />
+                    </>
+                )}
+                <div className="flex items-center space-x-2">
+                    <ThemeToggle />
+                </div>
+            </div>
+        </nav>
+    );
 }
