79f042e5fd24fdb7ed122a317302c26225744c6c
gautchap <gautchap+github@outlook.com>
2024-01-14 01:03:48 +0100

    refactor: login page and login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,20 @@
 import { LoginForm } from "@/components/login-form";
+import { Card, CardDescription, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
 export default async function Login() {
-  return (
-    <>
-      <section className="flex items-center h-[90dvh] justify-center">
-        <LoginForm />
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section className="flex items-center h-[90dvh] justify-center">
+                <Card>
+                    <CardHeader className="space-y-1">
+                        <CardTitle className="text-2xl">Se connecter</CardTitle>
+                        <CardDescription>Entrez votre adresse mail ci-dessous pour vous connecter</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <LoginForm />
+                    </CardContent>
+                </Card>
+            </section>
+        </>
+    );
 }
